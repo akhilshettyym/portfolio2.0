385db54b6d1236cf80a6305065143ca57236816c
akhilshettyym <akhilshettym2003@gmail.com>
2026-04-19 16:20:34 +0530

    basic code base initialized

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">Akhil's Portfolio</h1>
+    </main>
+  );
+}

471257a515f89dc9ddd9dc833d212c63b4125e13
Shetty, Akhil <AShetty3@globalindustrial.com>
2026-05-14 18:20:45 +0530

    tech stack replacement with bubblescene

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,15 +4,18 @@ import DevTicker from '@/components/DevTicker';
 import TechStack from '@/components/TechStack';
 import EducationLog from '@/components/EducationLog';
 import HeroSection from '@/components/HeroSection';
+import BubbleScene from '@/components/BubbleScene';
 
 const page = () => {
   return (
     <div>
-      <HeroSection />
-      <DevTicker />
-      <InfoLayout />
-      {/* <EducationLog />
-      <TechStack /> */}
+      {/* <HeroSection /> */}
+      {/* <DevTicker /> */}
+      {/* <InfoLayout /> */}
+      {/* <EducationLog /> */}
+      {/* <TechStack /> */}
+
+      <BubbleScene />
     </div>
   )
 }
